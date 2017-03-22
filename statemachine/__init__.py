3b4343501a17a1e01b835abc7ae97c5e83d39dad
Fernando Macedo <fgmacedo@gmail.com>
2017-03-22 10:51:29 -0300

    Bump version: 0.1.0 → 0.2.0

--- statemachine/__init__.py
+++ statemachine/__init__.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals, absolute_import
 
 __author__ = """Fernando Macedo"""
 __email__ = 'fgmacedo@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .statemachine import StateMachine, State, Transition
