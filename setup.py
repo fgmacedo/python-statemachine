70461fe9d9a9d2cf3432ac8a47ae0c9176f54ed4
Fernando Macedo <fgmacedo@gmail.com>
2017-07-10 18:35:20 -0300

    Bump version: 0.4.3 → 0.4.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='python-statemachine',
-    version='0.4.3',
+    version='0.4.4',
     description="Python Finite State Machines made easy.",
     long_description=readme + '\n\n' + history,
     author="Fernando Macedo",
--- statemachine/__init__.py
+++ statemachine/__init__.py
@@ -4,4 +4,4 @@ from .statemachine import StateMachine, State, Transition
 
 __author__ = """Fernando Macedo"""
 __email__ = 'fgmacedo@gmail.com'
-__version__ = '0.4.3'
+__version__ = '0.4.4'
