947b66c5b9f5086283969ace4ac547a3e21936d9
Fernando Macedo <fgmacedo@gmail.com>
2019-01-08 19:41:53 -0200

    fix registry loading modules for django 1.7+

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from __future__ import absolute_import, unicode_literals
 
+import mock
+import pytest
+
 
 def test_should_register_a_state_machine():
     from statemachine import StateMachine, State, registry
@@ -15,3 +18,31 @@ def test_should_register_a_state_machine():
 
     assert 'CampaignMachine' in registry._REGISTRY
     assert registry.get_machine_cls('CampaignMachine') == CampaignMachine
+
+
+@pytest.fixture()
+def django_module_loading():
+    import sys
+    if 'django' not in sys.modules:
+        django = mock.MagicMock()
+        module_loading = mock.MagicMock()
+        sys.modules['django'] = django
+        sys.modules['django.utils.module_loading'] = module_loading
+        yield module_loading
+        del sys.modules['django']
+        del sys.modules['django.utils.module_loading']
+
+
+def test_load_modules_should_call_autodiscover_modules(django_module_loading):
+    from statemachine.registry import load_modules
+
+    # given
+    modules = ['a', 'c', 'statemachine', 'statemachines']
+
+    # when
+    load_modules(modules)
+
+    # then
+    django_module_loading.autodiscover_modules.assert_has_calls(
+        mock.call(m) for m in modules
+    )
