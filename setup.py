a20dc92564dd07e9f36f48a431d133a81653c71d
Fernando Macedo <fgmacedo@gmail.com>
2019-01-17 16:18:59 -0200

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = []
 
 setup(
     name='python-statemachine',
-    version='0.7.0',
+    version='0.7.1',
     description="Python Finite State Machines made easy.",
     long_description=long_description,
     author="Fernando Macedo",
