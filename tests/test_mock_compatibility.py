d16ee1238dbe025eaf13c762a8c2e9c763c10e52
Fernando Macedo <fgmacedo@gmail.com>
2023-06-12 16:39:40 -0300

    fix: Add support for pytest-mock spy method fixing the SignatureAdapter (#392)

---
+++ tests/test_mock_compatibility.py
@@ -0,0 +1,25 @@
+from statemachine import State
+from statemachine import StateMachine
+
+
+def test_minimal(mocker):
+    class Observer:
+        def on_enter_state(self, event, model, source, target, state):
+            ...
+
+    obs = Observer()
+    on_enter_state = mocker.spy(obs, "on_enter_state")
+
+    class Machine(StateMachine):
+        a = State("Init", initial=True)
+        b = State("Fin")
+
+        cycle = a.to(b) | b.to(a)
+
+    state = Machine().add_observer(obs)
+    assert state.a.is_active
+
+    state.cycle()
+
+    assert state.b.is_active
+    on_enter_state.assert_called_once()
