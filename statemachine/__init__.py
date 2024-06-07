60221c39eb1d011f5e47b13da60768f8b524aa87
Fernando Macedo <fgmacedo@gmail.com>
2024-06-07 17:55:48 -0300

    chore: Bump version to 2.3.0

--- statemachine/__init__.py
+++ statemachine/__init__.py
@@ -3,6 +3,6 @@ from .statemachine import StateMachine
 
 __author__ = """Fernando Macedo"""
 __email__ = "fgmacedo@gmail.com"
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 
 __all__ = ["StateMachine", "State"]
