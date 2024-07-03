3afc6b65943842d1eaba6eabb6a56190d437fc6c
Fernando Macedo <fgmacedo@gmail.com>
2024-07-03 14:51:09 -0300

    chore: Release 2.3.3

--- statemachine/__init__.py
+++ statemachine/__init__.py
@@ -3,6 +3,6 @@ from .statemachine import StateMachine
 
 __author__ = """Fernando Macedo"""
 __email__ = "fgmacedo@gmail.com"
-__version__ = "2.3.2"
+__version__ = "2.3.3"
 
 __all__ = ["StateMachine", "State"]
