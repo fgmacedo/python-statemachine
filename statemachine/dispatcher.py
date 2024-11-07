9e54da7ca104da55149d795759bf11008f42a133
Fernando Macedo <fgmacedo@gmail.com>
2024-11-07 19:15:00 -0300

    refac: Turned search back to generators

--- statemachine/dispatcher.py
+++ statemachine/dispatcher.py
@@ -5,7 +5,6 @@ from operator import attrgetter
 from typing import TYPE_CHECKING
 from typing import Any
 from typing import Callable
-from typing import Dict
 from typing import Iterable
 from typing import List
 from typing import Set
@@ -26,23 +25,6 @@ if TYPE_CHECKING:
     from .callbacks import CallbackSpec
     from .callbacks import CallbackSpecList
     from .callbacks import CallbacksRegistry
-
-RegistryCallable = Callable[[str, "CallbackSpec", Callable[[], Callable]], None]
-
-
-class DictRegistry:
-    def __init__(self) -> None:
-        self._callbacks: Dict[str, Callable] = {}
-
-    def __call__(self, key: str, spec: "CallbackSpec", builder: Callable[[], Callable]) -> None:
-        if key not in self._callbacks:
-            callback = builder()
-            self._callbacks[key] = callback
-            callback.unique_key = key  # type: ignore[attr-defined]
-
-    @property
-    def callbacks(self) -> List[Callable]:
-        return list(self._callbacks.values())
 
 
 @dataclass
@@ -100,15 +82,15 @@ class Listeners:
                 continue
 
             executor = registry[specs.grouper(spec.group).key]
-            self.build(spec, registry=executor.add)
+            for key, builder in self.build(spec):
+                executor.add(key, spec, builder)
 
-    def _take_callback(
-        self, name: str, spec: "CallbackSpec", names_not_found_handler: Callable
-    ) -> Callable:
-        custom_registry = DictRegistry()
-
-        self.search_name(name, spec, custom_registry)
-        callbacks = custom_registry.callbacks
+    def _take_callback(self, name: str, names_not_found_handler: Callable) -> Callable:
+        callbacks: List[Callable] = []
+        for key, builder in self.search_name(name):
+            callback = builder()
+            callback.unique_key = key  # type: ignore[attr-defined]
+            callbacks.append(callback)
 
         if len(callbacks) == 0:
             names_not_found_handler(name)
@@ -118,7 +100,7 @@ class Listeners:
         else:
             return reduce(custom_and, callbacks)
 
-    def build(self, spec: "CallbackSpec", registry: RegistryCallable):
+    def build(self, spec: "CallbackSpec"):
         """
         Resolves the `spec` into callables in the `registry`.
 
@@ -127,14 +109,14 @@ class Listeners:
             registry (callable): A callable that will be used to store the resolved callables.
         """
         if not spec.may_contain_boolean_expression:
-            self.search(spec, registry=registry)
+            yield from self.search(spec)
             return
 
         # Resolves boolean expressions
 
         names_not_found: Set[str] = set()
         take_callback_partial = partial(
-            self._take_callback, spec=spec, names_not_found_handler=names_not_found.add
+            self._take_callback, names_not_found_handler=names_not_found.add
         )
 
         try:
@@ -147,19 +129,19 @@ class Listeners:
             spec.names_not_found = names_not_found
             return
 
-        registry(expression.unique_key, spec, lambda: expression)
+        yield expression.unique_key, lambda: expression
 
-    def search(self, spec: "CallbackSpec", registry: RegistryCallable):
+    def search(self, spec: "CallbackSpec"):
         if spec.reference is SpecReference.NAME:
-            self.search_name(spec.attr_name, spec, registry)
+            yield from self.search_name(spec.attr_name)
         elif spec.reference is SpecReference.CALLABLE:
-            self._search_callable(spec, registry)
+            yield from self._search_callable(spec)
         elif spec.reference is SpecReference.PROPERTY:
-            self._search_property(spec, registry)
+            yield from self._search_property(spec)
         else:  # never reached here from tests but put an exception for safety. pragma: no cover
             raise ValueError(f"Invalid reference {spec.reference}")
 
-    def _search_property(self, spec, registry: RegistryCallable):
+    def _search_property(self, spec):
         # if the attr is a property, we'll try to find the object that has the
         # property on the configs
         attr_name = spec.attr_name
@@ -168,28 +150,25 @@ class Listeners:
         for listener in self.items:
             func = getattr(type(listener.obj), attr_name, None)
             if func is not None and func is spec.func:
-                registry(
+                yield (
                     listener.build_key(attr_name),
-                    spec,
                     partial(attr_method, attr_name, listener.obj),
                 )
                 return
 
-    def _search_callable(self, spec, registry: RegistryCallable):
+    def _search_callable(self, spec):
         # if the attr is an unbounded method, we'll try to find the bounded method
         # on the self
         if not spec.is_bounded:
             for listener in self.items:
                 func = getattr(listener.obj, spec.attr_name, None)
                 if func is not None and func.__func__ is spec.func:
-                    registry(
-                        listener.build_key(spec.attr_name), spec, partial(callable_method, func)
-                    )
+                    yield listener.build_key(spec.attr_name), partial(callable_method, func)
                     return
 
-        registry(f"{spec.attr_name}@None", spec, partial(callable_method, spec.func))
+        yield f"{spec.attr_name}@None", partial(callable_method, spec.func)
 
-    def search_name(self, name, spec, registry: RegistryCallable):
+    def search_name(self, name):
         for listener in self.items:
             if name not in listener.all_attrs:
                 continue
@@ -197,14 +176,14 @@ class Listeners:
             key = listener.build_key(name)
             func = getattr(listener.obj, name)
             if not callable(func):
-                registry(key, spec, partial(attr_method, name, listener.obj))
+                yield key, partial(attr_method, name, listener.obj)
                 continue
 
             if isinstance(func, Event):
-                registry(key, spec, partial(event_method, func))
+                yield key, partial(event_method, func)
                 continue
 
-            registry(key, spec, partial(callable_method, func))
+            yield key, partial(callable_method, func)
 
 
 def callable_method(a_callable) -> Callable:
