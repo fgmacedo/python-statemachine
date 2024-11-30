ca3ecf70aadb5e4b08bf97cc8c6a4702e0c76f20
Fernando Macedo <fgmacedo@gmail.com>
2024-11-30 11:09:46 -0300

    refac: Attempt to use PriorityQueue

--- statemachine/engines/base.py
+++ statemachine/engines/base.py
@@ -1,4 +1,5 @@
-import heapq
+from queue import PriorityQueue
+from queue import Queue
 from threading import Lock
 from typing import TYPE_CHECKING
 from weakref import proxy
@@ -15,19 +16,46 @@ if TYPE_CHECKING:
 
 class BaseEngine:
     def __init__(self, sm: "StateMachine", rtc: bool = True):
-        self.sm: StateMachine = proxy(sm)
-        self._external_queue: list = []
         self._sentinel = object()
         self._rtc = rtc
-        self._processing = Lock()
         self._running = True
+        self._init(sm)
+
+    def _init(self, sm: "StateMachine"):
+        self.sm: StateMachine = proxy(sm)
+        self._external_queue: Queue = PriorityQueue()
+        self._processing = Lock()
+
+    def __getstate__(self) -> dict:
+        state = self.__dict__.copy()
+        del state["_external_queue"]
+        del state["_processing"]
+        del state["sm"]
+        return state
+
+    def __setstate__(self, state: dict) -> None:
+        for attr, value in state.items():
+            setattr(self, attr, value)
+
+    def empty(self):
+        return self._external_queue.qsize() == 0
 
     def put(self, trigger_data: TriggerData):
         """Put the trigger on the queue without blocking the caller."""
         if not self._running and not self.sm.allow_event_without_transition:
             raise TransitionNotAllowed(trigger_data.event, self.sm.current_state)
 
-        heapq.heappush(self._external_queue, trigger_data)
+        self._external_queue.put(trigger_data)
+
+    def pop(self):
+        try:
+            return self._external_queue.get(block=False)
+        except Exception:
+            return None
+
+    def clear(self):
+        with self._external_queue.mutex:
+            self._external_queue.queue.clear()
 
     def start(self):
         if self.sm.current_state_value is not None:
