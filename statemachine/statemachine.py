e16e0ef548bc0d6d2dae582dd62ef2d659d44cea
Fernando Macedo <fgmacedo@gmail.com>
2024-11-30 08:35:29 -0300

    feat: Supporting pickle protocol (#500)

--- statemachine/statemachine.py
+++ statemachine/statemachine.py
@@ -147,6 +147,27 @@ class StateMachine(metaclass=StateMachineMetaclass):
         cp.add_listener(*cp._listeners.keys())
         return cp
 
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state["_rtc"] = self._engine._rtc
+        del state["_callbacks"]
+        del state["_states_for_instance"]
+        del state["_engine"]
+        return state
+
+    def __setstate__(self, state):
+        listeners = state.pop("_listeners")
+        rtc = state.pop("_rtc")
+        self.__dict__.update(state)
+        self._callbacks = CallbacksRegistry()
+        self._states_for_instance: Dict[State, State] = {}
+
+        self._listeners: Dict[Any, Any] = {}
+
+        self._register_callbacks([])
+        self.add_listener(*listeners.keys())
+        self._engine = self._get_engine(rtc)
+
     def _get_initial_state(self):
         initial_state_value = self.start_value if self.start_value else self.initial_state.value
         try:
