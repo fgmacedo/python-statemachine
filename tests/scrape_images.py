9a089edf61b4afd4abc08968b3cfa508fa332f7c
Oliver Davey <o.p.davey@gmail.com>
2025-07-01 15:44:20 +0100

    Fix: Sanitise project path on windows for doc build (#527)
    * Sanitise project path on windows for doc build
    * MachineScraper now uses regex escape function for robustness

--- tests/scrape_images.py
+++ tests/scrape_images.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 from statemachine.contrib.diagram import DotGraphMachine
@@ -13,7 +14,8 @@ class MachineScraper:
 
     def __init__(self, project_root):
         self.project_root = project_root
-        self.re_machine_module_name = re.compile(f"{self.project_root}/(.*).py$")
+        sanitized_path = re.escape(os.path.abspath(self.project_root))
+        self.re_machine_module_name = re.compile(f"{sanitized_path}/(.*)\\.py$")
         self.seen = set()
 
     def __repr__(self):
